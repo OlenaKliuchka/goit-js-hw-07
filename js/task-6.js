2564463e092326e6492bdafbcb227df206abf34c
OlenaKliuchka <126958363+OlenaKliuchka@users.noreply.github.com>
2024-04-12 19:19:11 +0300

    finish

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divEl = document.querySelector('#controls');
+const inputEl = divEl.firstElementChild;
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  boxesEl.innerHTML = '';
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesEl.appendChild(box);
+    boxSize += 10;
+  }
+}
+
+function handleCreate() {
+  const amount = parseInt(inputEl.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    inputEl.value = '';
+  }
+}
+
+function handleDestroy() {
+  destroyBoxes();
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+}
+
+btnCreateEl.addEventListener('click', handleCreate);
+btnDestroyEl.addEventListener('click', handleDestroy);
