a1426bd2addd6c2b3840080f97503b47ac33122c
ElenaKliuchka <klychikel@gmail.com>
2024-04-12 11:24:49 +0300

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+const colorOutput = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorOutput.textContent = color;
+}
